1f93d4b0d59f733f9f556ce09503a1ffb571fe8f
Edward Yuen <edwardyuensf@gmail.com>
2019-12-11 20:07:23 -0800

    First commit, sample blog posts

---
+++ edward_yuen_blog/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+posts = [
+	{
+		'author': 'Edward Yuen',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'December 10, 2019'
+	},
+	{
+		'author': 'Vivian Mei',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'December 11, 2019'
+	}
+]
+
+def home(request):
+	context ={
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html')
