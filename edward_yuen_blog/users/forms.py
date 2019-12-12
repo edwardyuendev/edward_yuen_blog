74ee0dcf45aef91f52ba860b7fa31f48bd43d529
Edward Yuen <edwardyuensf@gmail.com>
2019-12-11 20:07:23 -0800

    added user/profile update capabilities

--- edward_yuen_blog/users/forms.py
+++ edward_yuen_blog/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # User Registration form that inherits from UserCreationForm
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +12,16 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+
+	# Form that will update our User Model
+	class Meta:
+		model = User
+		fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']
